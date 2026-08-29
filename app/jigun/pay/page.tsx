77c7925d27fc67834b3077ce8b9279654af6af5e
godplan-hue <junga6783@gmail.com>
2026-08-29 15:18:11 +0900

    결제보안: jigun/pay paymentId 일치검증 추가
    이미 pendingInfo에 paymentId가 저장돼 있어서, URL paymentId와 다르면
    완료처리 안하도록 검증 한 줄만 추가. 정상 결제 흐름엔 영향 없음.

--- app/jigun/pay/page.tsx
+++ app/jigun/pay/page.tsx
@@ -89,6 +89,7 @@ function PayInner() {
     }
     try {
       const info = JSON.parse(pendingRaw);
+      if (info.paymentId && info.paymentId !== pgPaymentId) return;
       finalizeSuccess(info);
     } catch {}
     // eslint-disable-next-line react-hooks/exhaustive-deps
